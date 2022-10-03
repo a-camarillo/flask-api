aee84c1db4883a6461e3d7434aba8c494893026d
a-camarillo <anthony.camarillo.96@gmail.com>
2022-10-02 17:32:58 -0700

    initial code setup for flask server

---
+++ server/app/server/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__,instance_relative_config=True)
+  
+
+    @app.route('/')
+    def hello():
+        return {
+            'user': 'MF DOOM',
+            'nickname': 'Viktor Vaughn'
+        }
+    
+    return app
