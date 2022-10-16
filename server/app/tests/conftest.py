3d7e84b91e7e34de272f014ed691c71b205ccec1
a-camarillo <anthony.camarillo.96@gmail.com>
2022-10-16 09:03:25 -0700

    Create configuration setup for testing using pytest

--- server/app/tests/conftest.py
+++ server/app/tests/conftest.py
@@ -1,10 +1,41 @@
 import os
+
 import pytest
 
 from server import create_app
-from server.models import Beer, Brand
+from server.models import Beer, Brand, db
+
+from data import populate_db
+
+@pytest.fixture()
+def app():
+
+    app = create_app()
+    app.config['TESTING'] = True
+
+    db.init(app)
+    # creating a new database for the unit test
+    with app.app_context():
+        db.create_all()
+        populate_db(db)
+    
+    yield app
+
+    # tear down the database
+    with app.app_context():
+        db.drop_all()
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
 
 @pytest.fixture(scope='module')
 def new_beer():
-    beer = Beer('Wahoo','Ballast Point',4.5)
+    beer = Beer(name='Wahoo',brand='Ballast Point',abv=4.5)
     return beer
+
+@pytest.fixture(scope='module')
+def new_brand():
+    brand = Brand(name='Alamo')
+    return brand
