daef8afa035c4f405bf067c7b0446e54a029713f
a-camarillo <anthony.camarillo.96@gmail.com>
2022-10-16 09:05:11 -0700

    Add testing for Beer and Brand Models

--- server/app/tests/unit/test_models.py
+++ server/app/tests/unit/test_models.py
@@ -1,9 +1,17 @@
-def test_new_beer(new_beer)
-""" 
-GIVEN a Beer model
-WHEN a new Beer is created
-THEN check the name, brand, and abv are defined correctly
-"""
-assert new_beer.name == 'Wahoo'
-assert new_beer.brand == 'Ballast Point'
-assert new_beer.abv == 4.5
+def test_new_beer(new_beer):
+    """ 
+    GIVEN a Beer model
+    WHEN a new Beer is created
+    THEN check the name, brand, and abv are defined correctly
+    """
+    assert new_beer.name == 'Wahoo'
+    assert new_beer.brand == 'Ballast Point'
+    assert new_beer.abv == 4.5
+
+def test_new_brand(new_brand):
+    """
+    GIVEN a Brand model
+    WHEN a new Brand is created
+    THEN check the name, brand, and abv are defined correctly
+    """
+    assert new_brand.name == 'Alamo'
