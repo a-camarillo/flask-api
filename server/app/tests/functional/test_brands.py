20f2c3022cffc1723dd425ce4b849d1f8e3e8be3
a-camarillo <anthony.camarillo.96@gmail.com>
2022-10-18 17:03:52 -0700

    create package for functional tests and module for testing brands

---
+++ server/app/tests/functional/test_brands.py
@@ -0,0 +1,10 @@
+def test_brands(client):
+    """
+    GIVEN a Flask app
+    WHEN the route '/brands/' is requested (GET)
+    THEN check the response is valid
+    """
+    response = client.get('/brands')
+    assert response.status_code == 200
+    print(response.data)
+    assert response.data
