1ec83ae81279d504e6cbe553e4cdc8b984cfc8da
a-camarillo <anthony.camarillo.96@gmail.com>
2022-10-22 14:31:33 -0700

    Add unique constraint to brands.name

--- server/app/server/models.py
+++ server/app/server/models.py
@@ -3,16 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 db=SQLAlchemy()
 
-class Beer(db.Model):
-    __tablename__ = 'beer'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    brand = db.Column(db.String, db.ForeignKey('brand.name'))
-    abv = db.Column(db.Float)
-
 class Brand(db.Model):
-    __tablename__ = 'brand'
+
+    __tablename__ = 'brands'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+
+    __tableargs__ = db.UniqueConstraint('id', 'name', name="unique_brand")
+
+class Beer(db.Model):
+    __tablename__ = 'beers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    brand = db.Column(db.String, db.ForeignKey('brands.name'))
+    abv = db.Column(db.Float)
