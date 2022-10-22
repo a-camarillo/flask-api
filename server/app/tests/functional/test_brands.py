30c183ab9aea1c52e3ead53512f462a9da670184
a-camarillo <anthony.camarillo.96@gmail.com>
2022-10-22 14:30:19 -0700

    Add trailing forward slash to get route to avoid 308 response for permanent redirect

--- server/app/tests/functional/test_brands.py
+++ server/app/tests/functional/test_brands.py
@@ -4,7 +4,7 @@ def test_brands(client):
     WHEN the route '/brands/' is requested (GET)
     THEN check the response is valid
     """
-    response = client.get('/brands')
+    response = client.get('/brands/')
     assert response.status_code == 200
     print(response.data)
     assert response.data
